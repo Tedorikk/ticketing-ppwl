0bf4372c2501cb3de588eb0749760499a2e7d45d
KarenTrixie <karentrixie7@gmail.com>
2025-05-16 16:53:22 +0700

    bikin layout hlmn user

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,14 @@ Route::get('/', function () {
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
+    Route::get('admin/dashboard', function () {
         return Inertia::render('dashboard');
+    })->name('dashboard');
+});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('user/dashboard');
     })->name('dashboard');
 });
 
